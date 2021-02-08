ba1cf0453252b7ae587d3718df3ab8a52360f2b6
Wright <calebogundiya@gmail.com>
2021-02-08 10:35:41 +0100

    ErrorBoundary componenet for handling component errors gracefully

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
+import SearchParams from './SearchParams';
+import Details from "./Details";
+
+const App = () => {
+	return (
+		<React.StrictMode>
+			<div>
+				<header>
+					<Link to="/">
+						Adopt Me!
+					</Link>
+				</header>
+				<Router>
+					<SearchParams path="/"/>
+					<Details path="/details/:id"/>
+				</Router>
+			</div>
+		</React.StrictMode>
+	);
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/Details.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import pet from '@frontendmasters/pet';
+import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
+
+class Details extends React.Component {
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {
+    //         loading: true
+    //     }
+    // }
+
+    state = { loading: true };
+
+    componentDidMount() {
+        pet.animal(this.props.id)
+            .then(({ animal }) => {
+                this.setState({
+                    name: animal.name,
+                    animal: animal.type,
+                    location: `${animal.contact.address.city}, ${animal.contact.address.address1}`,
+                    description: animal.description,
+                    media: animal.photos,
+                    breed: animal.breeds.primary,
+                    loading: false
+                })
+            }, console.error);
+    }
+
+    render() {
+        if (this.state.loading) {
+            return <h1>loading ...</h1>
+        }
+
+        const { animal, breed, location, description, name, media } = this.state;
+
+        return (
+            <div className="details">
+                <Carousel media={media} />
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+// const Details = (props) => {
+//     return (
+//         <pre>
+//             <code>
+//                 {JSON.stringify(props, null, 4)}
+//             </code>
+//         </pre>
+//     )
+// }
+
+export default function DetailsWithErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <Details {...props} />
+        </ErrorBoundary>
+    )
+}
